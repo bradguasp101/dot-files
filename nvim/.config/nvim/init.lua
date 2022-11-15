c78f7ea5f26ecab6656e6216b77cb162675e50c2
Ed <ed.kilgore@tcnbroadcasting.com>
2022-11-15 12:08:46 -0700

    Enable profiling

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,9 @@
 CONFIG_PATH = vim.fn.stdpath('config')
 DATA_PATH = vim.fn.stdpath('data')
 
-pcall(require, 'impatient')
+local impatient_ok, impatient = pcall(require, 'impatient')
+if impatient_ok then
+  impatient.enable_profile()
+end
+
 require "kobra"
