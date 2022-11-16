3ce07d3343672edb7741396de5d3d9bb910023fa
Ed <ed.kilgore@tcnbroadcasting.com>
2022-11-16 07:52:49 -0700

    Update clangd server to avoid formatting protobufs

--- nvim/.config/nvim/lua/kobra/lsp/settings/clangd.lua
+++ nvim/.config/nvim/lua/kobra/lsp/settings/clangd.lua
@@ -1,5 +1,8 @@
 return {
   cmd = {DATA_PATH .. '/mason/bin/clangd'},
+  filetypes = {
+    "c", "cpp", "objc", "objcpp", "cuda", -- don't wan't protos to get aggressively reformatted
+  },
   handlers = {
     ['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
       virtual_text = {spacing = 0, prefix = ''},
