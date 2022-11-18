438b7cc0a515ce94add2e0a4743fe1877d9af351
Ed <ed.kilgore@tcnbroadcasting.com>
2022-11-18 09:07:15 -0700

    Add custom git spaceship prompt

---
+++ spaceship/.config/spaceship/git.zsh
@@ -0,0 +1,1 @@
+SPACESHIP_GIT_ORDER=(short_branch git_status)
--- spaceship/.spaceshiprc.zsh
+++ spaceship/.spaceshiprc.zsh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+for conf in "$HOME/.config/spaceship/"*.zsh; do
+  source "${conf}"
+done
+unset conf
+
 SPACESHIP_PROMPT_ADD_NEWLINE=false
 SPACESHIP_PROMPT_SEPARATE_LINE=true
 
 SPACESHIP_PROMPT_ORDER=(
   dir
+  short_branch
   git
   gcloud
   exec_time
