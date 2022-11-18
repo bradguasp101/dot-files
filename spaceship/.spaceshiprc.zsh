b3e8b77f545165beeb9eb956d7a12e961d67755b
Ed <ed.kilgore@tcnbroadcasting.com>
2022-11-18 07:59:27 -0700

    Update spaceship prompt

--- spaceship/.spaceshiprc.zsh
+++ spaceship/.spaceshiprc.zsh
@@ -2,3 +2,16 @@
 
 SPACESHIP_PROMPT_ADD_NEWLINE=false
 SPACESHIP_PROMPT_SEPARATE_LINE=true
+
+SPACESHIP_PROMPT_ORDER=(
+  dir
+  git
+  gcloud
+  exec_time
+  async
+  line_sep
+  jobs
+  exit_code
+  sudo
+  char
+)
