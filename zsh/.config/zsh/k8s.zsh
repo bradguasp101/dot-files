4e429f3921ffcd8083fd1945da644676ff8ac490
Ed <ed.kilgore@tcnbroadcasting.com>
2022-11-18 09:15:34 -0700

    Update port forward script

--- zsh/.config/zsh/k8s.zsh
+++ zsh/.config/zsh/k8s.zsh
@@ -27,7 +27,12 @@ alias yul='switch_context yul yul-1 yul'
 # Port forward a pod, `pfs service` will port-forward service-xxxxxxxxxx-xxxxx at localhost:50051
 pfs() {
   POD=$(kubectl -n default get pods | grep -E "^$1-\S{10}-\S{5}\s+(.*?)$" | sed 1q | awk '{print $1;}')
-  PORT="50051:50051"
+  if [[ $2 -eq 0 ]]; then
+    echo -e "\e[33mNo port specified, defaulting to 50051...\e[0m"
+    PORT="50051:50051"
+  else
+    PORT="$2:$2"
+  fi
   echo -e "\e[36m\e[1mForwarding $POD\e[0m"
   kubectl -n default port-forward $POD $PORT
 }
