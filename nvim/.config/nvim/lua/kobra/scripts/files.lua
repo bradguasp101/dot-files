58019427872cd243a99fca308252f2a9bb887d50
Ed <ed.kilgore@tcnbroadcasting.com>
2022-11-30 09:21:28 -0700

    Update revive script

--- nvim/.config/nvim/lua/kobra/scripts/files.lua
+++ nvim/.config/nvim/lua/kobra/scripts/files.lua
@@ -1,16 +1,26 @@
 local files = {}
 
+-- will return the root of the git repository, otherwise just returns the directory
+-- the current file is in
 function files.getRoot()
-  return vim.fn.fnamemodify(vim.fn.expand('%:h'), ':~:.')
+  local exists = os.execute('git -C . rev-parse 2>/dev/null')
+  if exists ~= 0 then
+    return vim.fn.fnamemodify(vim.fn.expand('%:h'), ':~:.')
+  end
+
+  local handle = io.popen('git rev-parse --show-toplevel')
+  if handle == nil then
+    return vim.fn.fnamemodify(vim.fn.expand('%:h'), ':~:.')
+  end
+
+  local result = handle:read('*a')
+  handle:close()
+
+  return result
 end
 
 function files.getRelativeFile()
-  return vim.fn.fnamemodify(vim.fn.expand('%:'), ':p~:.')
-end
-
-function files.exists(name)
-  local f = io.open(name, 'r')
-  if f ~= nil then io.close(f) return true else return false end
+  return vim.fn.fnamemodify(vim.fn.expand('%:'), ':p:~:.')
 end
 
 return files
--- nvim/.config/nvim/lua/kobra/scripts/revive.lua
+++ nvim/.config/nvim/lua/kobra/scripts/revive.lua
@@ -1,15 +1,12 @@
 local revive = {}
 
+-- this assumes that you're in a git repository, and that you have a revive
+-- config.toml file in the root directory
 function revive.lint()
   local files = require('kobra.scripts.files')
-  local cmd = 'belowright split | resize 20 | term revive'
+  local cmd = 'belowright split | resize 20 | term revive '
   local root = files.getRoot()
-
-  if files.exists(root .. '/config.toml') then
-    cmd = cmd .. ' --config ' .. root .. '/config.toml '
-  end
-
-  vim.api.nvim_command(cmd .. ' --formatter stylish ' .. files.getRelativeFile())
+  vim.api.nvim_command(cmd .. ' --config ' .. root .. '/config.toml --formatter stylish ' .. files.getRelativeFile())
 end
 
 return revive
