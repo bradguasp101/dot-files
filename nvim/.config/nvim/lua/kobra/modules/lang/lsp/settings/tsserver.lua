f1cca020337046c2ab7a2c33588470d5d10276e0
Ed <ed.kilgore@tcnbroadcasting.com>
2022-12-07 12:39:16 -0700

    Update tsserver config

--- nvim/.config/nvim/lua/kobra/modules/lang/lsp/settings/tsserver.lua
+++ nvim/.config/nvim/lua/kobra/modules/lang/lsp/settings/tsserver.lua
@@ -2,7 +2,13 @@ return {
   cmd = {require('kobra.modules.lang.lsp.data').path .. '/bin/typescript-language-server', '--stdio'},
   filetypes = {'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx'},
   root_dir = require('lspconfig/util').root_pattern('package.json', 'tsconfig.json', 'jsconfig.json', '.git'),
-  settings = {documentFormatting = true},
+  settings = {
+    documentFormatting = true,
+    maxTsServerMemory = 4096,
+    preferences = {
+      quotePreference = 'single',
+    },
+  },
   handlers = {
     ['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
       virtual_text = {spacing = 0, prefix = ""},
