a2e0eccc17da1f06f971c6d785b2b103dcdf7ce7
Ed <ed.kilgore@tcnbroadcasting.com>
2023-02-21 16:58:59 -0700

    Add grpcc command

---
+++ fish/.config/fish/functions/grpcc.fish
@@ -0,0 +1,35 @@
+#!/usr/bin/env fish
+
+function grpcc -d "makes a request to the selected method from grpclist, or lists available services/methods"
+  if test -z "$argv[1]"
+    echo -e "\e[31mUsage: grpclist port [service-num]\e[0m"
+    return 1
+  end
+
+  if test -z "$argv[2]"
+    grpcurl -plaintext localhost:$argv[1] list | awk '{print NR,$0}'
+    return 0
+  end
+
+  set services (grpcurl -plaintext localhost:$argv[1] list)
+  if test "$argv[2]" -gt (count $services); or test "$argv[2]" -le 0
+    echo -e "\e[31mInvalid service number\e[0m"
+    return 1
+  end
+
+  if test -z "$argv[3]"
+    grpcurl -plaintext localhost:$argv[1] list $services[$argv[2]] | awk '{print NR,$0}'
+    return 0
+  end
+
+  set service $services[$argv[2]]
+  set methods (grpcurl -plaintext localhost:$argv[1] list $service)
+  if test "$argv[3]" -gt (count $methods); or test "$argv[3]" -le 0
+    echo -e "\e[31mInvalid method number\e[0m"
+    return 1
+  end
+
+  set methods (string split . $methods[$argv[3]])
+  set method $methods[-1]
+  grpcurl -plaintext $argv[4..-1] localhost:$argv[1] $service/$method
+end
