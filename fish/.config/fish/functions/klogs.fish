60aaa3c594783b104e5c225530d5098314ebfce0
Ed <ed.kilgore@tcnbroadcasting.com>
2023-02-21 15:15:13 -0700

    Update klogs to print logs for crashlooping pod

--- fish/.config/fish/functions/klogs.fish
+++ fish/.config/fish/functions/klogs.fish
@@ -1,20 +1,16 @@
 #!/usr/bin/env fish
 
 function klogs
-  if set -q "$argv"
-    echo -e "\e[31mUsage: klogs pod-name\e[0m"
-    return 1
-  end
-
   if test -z "$argv[1]"
     echo -e "\e[31mUsage: klogs pod-name\e[0m"
     return 1
   end
 
-  set pods (k get pods | grep $argv[1] | awk '{print $1;}')
+  set pods (k get pods | grep $argv[1] | awk '$3 == "CrashLoopBackOff" {print $1;}')
   if set -q "$pods"
     echo -e "\e[31mNo pods found for that name\e[0m"
+    return 1
   end
 
-  k logs "$pods[1]" "$argv[1]"
+  k logs $pods[1] $argv[1]
 end
