96ec85a9a1668c9ac3714d9a8c48668168600aa7
Ed <ed.kilgore@tcnbroadcasting.com>
2023-02-21 15:23:52 -0700

    Update klogs

--- fish/.config/fish/functions/klogs.fish
+++ fish/.config/fish/functions/klogs.fish
@@ -1,16 +1,25 @@
 #!/usr/bin/env fish
 
-function klogs
+function klogs -d "print logs for a pod given the container name, assuming the pod and container share the same name"
   if test -z "$argv[1]"
-    echo -e "\e[31mUsage: klogs pod-name\e[0m"
+    echo -e "\e[31mUsage: klogs pod-name [-c]\e[0m"
     return 1
   end
 
-  set pods (k get pods | grep $argv[1] | awk '$3 == "CrashLoopBackOff" {print $1;}')
+  if test -z "$argv[2]"
+    set pods (k get pods | grep $argv[1] | awk '{print $1;}')
+  else if test "$argv[2]" = "-c"
+    set pods (k get pods | grep $argv[1] | awk '$3 == "CrashLoopBackOff" {print $1;}')
+  else
+    echo -e "\e[31mUsage: klogs pod-name [-c]\e[0m"
+    return 1
+  end
+
   if set -q "$pods"
     echo -e "\e[31mNo pods found for that name\e[0m"
     return 1
   end
 
+  echo -e "\e[32mk logs $pods[1] $argv[1]\e[0m"
   k logs $pods[1] $argv[1]
 end
