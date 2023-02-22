c155b1f8adad93dc0c52fee15105c6f52a08d23d
Ed <ed.kilgore@tcnbroadcasting.com>
2023-02-22 08:29:23 -0700

    Update pf commands

--- fish/.config/fish/functions/pfui.fish
+++ fish/.config/fish/functions/pfui.fish
@@ -2,7 +2,7 @@
 
 function pfui -d "port forward a service and grpcui connect to it"
   if set -q "$argv"; or test -z "$argv[1]"
-    echo -e "\e[31mNo service specified, exiting...\e[0m"
+    echo -e "\e[31mUsage: pfui service [port]\e[0m"
     return 1
   end
 
@@ -13,13 +13,13 @@ function pfui -d "port forward a service and grpcui connect to it"
     set port "$argv[2]"
   end
 
-  echo -e "\e[36m\e[1mForwarding $argv[1]\e[0m"
-  kubectl -n default port-forward services/$argv[1] "$port:$port" &
+  echo -e "\e[36mk -n default port-forward services/$argv[1] $port:$port\e[0m"
+  k -n default port-forward services/$argv[1] "$port:$port" &
 
   set pid (jobs -l | awk '{print $2}')
-  echo -e "\e[32m\e[1mForwarded $argv[1] on process $pid\e[0m"
+  echo -e "\e[32mForwarded $argv[1] on process $pid\e[0m"
 
   # TODO: auto kill process
-  sleep 1
+  sleep 2
   grpcui --plaintext "localhost:$port"
 end
