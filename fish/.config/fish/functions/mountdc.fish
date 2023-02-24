8f3ff786f540d9ed2bd4e55f2161555564f43c27
Ed <ed.kilgore@tcnbroadcasting.com>
2023-02-23 17:20:52 -0700

    Fix mountdc function

--- fish/.config/fish/functions/mountdc.fish
+++ fish/.config/fish/functions/mountdc.fish
@@ -1,14 +1,17 @@
 #!/usr/bin/env fish
 
 function mountdc -d "exec into the devclone pod"
+  if test -z "$argv[1]"
+    echo -e "\e[31mUsage: mountdc service\e[0m"
+    return 1
+  end
+
   set name dev-(whoami | tr '.' '-')
-  set pod (kubectl -n default get pods | grep -E $name | sed 1q | awk '{print $1;}')
+  set pod (k -n default get pods | grep -E $name | sed 1q | awk '{print $1;}')
   if test -z $pod
     echo -e "\e[1m\e[31mNo devclone pod found for $(whoami | tr '.' '-'), exiting\e[0m"
     return 1
   end
 
-  set name_length (math (string length $name) + 2)
-  set container (string sub -s $name_length -e -17 $pod)
-  kubectl exec -n default $pod -c $container -it -- zsh
+  kubectl exec -n default $pod -c $argv[1] -it -- zsh
 end
