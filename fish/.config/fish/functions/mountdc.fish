4677f17d88289d739ae4b1c4cc9e9ccfda9a7a34
Ed <ed.kilgore@tcnbroadcasting.com>
2023-03-09 13:14:04 -0700

    Update mountdc command

--- fish/.config/fish/functions/mountdc.fish
+++ fish/.config/fish/functions/mountdc.fish
@@ -1,17 +1,5 @@
 #!/usr/bin/env fish
 
 function mountdc -d "exec into the devclone pod"
-  if test -z "$argv[1]"
-    echo -e "\e[31mUsage: mountdc service\e[0m"
-    return 1
-  end
-
-  set name dev-(whoami | tr '.' '-')
-  set pod (k -n default get pods | grep -E $name | sed 1q | awk '{print $1;}')
-  if test -z $pod
-    echo -e "\e[1m\e[31mNo devclone pod found for $(whoami | tr '.' '-'), exiting\e[0m"
-    return 1
-  end
-
-  kubectl exec -n default $pod -c $argv[1] -it -- zsh
+  k exec (kp dev-(whoami | tr '.' '-')) -it -- /bin/bash
 end
