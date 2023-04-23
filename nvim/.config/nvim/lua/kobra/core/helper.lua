53a68f1c71960c361b9a6bda1600048ea1a36c5a
Ed <ed.kilgore@tcnbroadcasting.com>
2023-04-23 15:34:01 -0600

    Update plugins

--- nvim/.config/nvim/lua/kobra/core/helper.lua
+++ nvim/.config/nvim/lua/kobra/core/helper.lua
@@ -1,6 +1,7 @@
-_G = _G or {}
-return {
-  init = function()
-    -- do stuff
-  end
-}
+local helper = {}
+
+helper.init = function()
+  vim.api.nvim_create_user_command('UpdateLazy', require('kobra.core.plugins').update_lazy, {})
+end
+
+return helper
--- nvim/.config/nvim/lua/kobra/core/init.lua
+++ nvim/.config/nvim/lua/kobra/core/init.lua
@@ -6,28 +6,13 @@ local leader_map = function()
   vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true })
 end
 
-local load_lazy = function()
-  local lazypath = global.data_path .. '/lazy/lazy.nvim'
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable',
-      lazypath,
-    })
-  end
-  vim.opt.rtp:prepend(lazypath)
-end
-
 local load_core = function()
   require('kobra.core.helper').init()
 
   leader_map()
   vim.cmd([[set shell=/usr/local/bin/fish]])
 
-  load_lazy()
+  require('kobra.core.plugins').load_lazy()
   require('kobra.core.options')
   require('kobra.core.mapping')
   require('kobra.core.events').setup()
