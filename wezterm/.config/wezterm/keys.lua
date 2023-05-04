df77c584ddd86edd68d562fb97bfd831e2e1a572
Ed <ed.kilgore@tcnbroadcasting.com>
2023-05-03 20:07:44 -0600

    Update wezterm to use image background

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -5,6 +5,7 @@ function M.apply(config)
   config.send_composed_key_when_left_alt_is_pressed = false
   config.send_composed_key_when_right_alt_is_pressed = false
   config.use_ime = false
+  config.disable_default_key_bindings = true
 end
 
 return M
--- wezterm/.config/wezterm/ui.lua
+++ wezterm/.config/wezterm/ui.lua
@@ -2,14 +2,33 @@ local M = {}
 
 local wezterm = require('wezterm')
 
+local function background(config)
+  local home = os.getenv('HOME')
+
+  config.background = {
+    {
+      source = {
+        File = home .. '/.config/wezterm/images/chaotic_gore_magala.jpeg',
+      },
+      hsb = {
+        brightness = 0.05,
+      },
+    },
+  }
+end
+
 function M.apply(config)
-  config.color_scheme = 'Dark+'
-  config.font = wezterm.font('GoMono Nerd Font Mono')
-  config.font_size = 16.0
-  config.window_background_opacity = 0.9
   config.enable_tab_bar = false
+  config.window_background_opacity = 0.9
   config.initial_rows = 50
   config.initial_cols = 160
+
+  config.font = wezterm.font('GoMono Nerd Font Mono')
+  config.font_size = 16.0
+
+  config.color_scheme = 'Dark+'
+
+  background(config)
 end
 
 return M
